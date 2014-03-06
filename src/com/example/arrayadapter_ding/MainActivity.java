ee21c5c56ba83df32d90f82a12e6b133f5ff4087
fannyting <fannyting0227@gmail.com>
2014-03-06 22:04:40 +0800

    one

---
+++ src/com/example/arrayadapter_ding/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.arrayadapter_ding;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	private ListView listView;
+	private String[] data={"A","B","C","D","E","F","G","H","I","J","K","L",};
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        listView = (ListView) findViewById(R.id.listView1);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data));
+        
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+			
+				Log.e("TAG",""+arg3);
+				
+			}
+			
+			
+		});
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
